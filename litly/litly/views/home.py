3e5af16cad23bdb283bf3381eb5d6fdbd013c844
manducku <alwaysfun2183@gmail.com>
2016-03-16 02:18:05 +0900

    Home View post function add

--- litly/litly/views/home.py
+++ litly/litly/views/home.py
@@ -1,8 +1,9 @@
 from django.views.generic import View,TemplateView,CreateView 
 from resources.models import Resource 
-from django.shortcuts import render 
+from django.core.urlresolvers import reverse
+from django.shortcuts import render, redirect
+
 class HomeView(View):
-    
     def get(self, request):
         context = {
                 'resources': Resource.objects.all()
@@ -14,6 +15,13 @@ class HomeView(View):
                 context = context,
                 )
 
-        
-
+    def post(self, request):
+        Resource.objects.create(
+                prev_url = request.POST.get("prev_url")
+                )
+        return redirect(
+                reverse(
+                    "home"
+                    )
+                )
 
