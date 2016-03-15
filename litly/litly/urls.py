20395fd8cefb42104dcc9193a7de98aa0002839e
manducku <alwaysfun2183@gmail.com>
2016-03-16 00:03:33 +0900

    home --> homeView.as_view() change

--- litly/litly/urls.py
+++ litly/litly/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from litly.views import home 
+from litly.views import HomeView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^/$', home),
+    url(r'^/$', HomeView.as_view(), name = "home"),
 ]
