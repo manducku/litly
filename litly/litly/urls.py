949cf043bcd5149661deee51486dac8e5c56fbd5
manducku <alwaysfun2183@gmail.com>
2016-03-16 00:07:16 +0900

    remove / in url name home

--- litly/litly/urls.py
+++ litly/litly/urls.py
@@ -19,5 +19,5 @@ from litly.views import HomeView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^/$', HomeView.as_view(), name = "home"),
+    url(r'^$', HomeView.as_view(), name = "home"),
 ]
