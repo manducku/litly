3a75084f2408487ac2ea88c105b9b07a6b29cb28
manducku <alwaysfun2183@gmail.com>
2016-03-16 01:35:30 +0900

    from change

--- litly/litly/views/home.py
+++ litly/litly/views/home.py
@@ -1,5 +1,14 @@
-from django.views.generic import View,TemplateView 
+from django.views.generic import View,TemplateView,CreateView 
+from resources.models import Resource 
 
-class HomeView(TemplateView):
+class HomeView(TemplateView, CreateView):
     template_name = "home.html"
+    fields = ['prev_url', ]
+
+    def get_context_data(self):
+        context_data = super(HomeView, self).get_context_data()
+        context_data['resources']= Resource.objects.all()
+
+        return context_data
+
 
